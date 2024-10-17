5138f3ab142b0371daa108cfa2a40ecb9e66a091
Adrian Bengoechea <ebel.bengoechea@gmail.com>
2024-10-17 15:52:57 -0600

    Advanced background settings fix

--- inc/regular-content-helpers.php
+++ inc/regular-content-helpers.php
@@ -9,6 +9,7 @@ function regular_content_background( $background = false, $settings = [] ){
     $color = $background['color'];
     
 
+    $content = '';
     $desktop_image = '';
 
     
@@ -27,21 +28,61 @@ function regular_content_background( $background = false, $settings = [] ){
         $mobile_image = $background['image']['mobile']['url'];
     }
 
-    $video_url = $background['video']['url'];
+
+
+    $video = $background['video'];
+
+    $video_enabled = $video['enable_background_video'];
+    $video_type = $video['video_type'];
+    $video_media_file = $video['media_file'];
+    $video_embed_url = $video['embed_url'];
+
+
+
     $overlay = $background['overlay'];
 
     $parallax = ( isset($settings['parallax']) ) ? $settings['parallax'] : false;
 
-    $content = '
+    $content .= '
         <div class="regular-content__background ' . ( ( $parallax ) ? 'regular-content__background--parallax' : '' ) . '">
             <div class="regular-content__background__wrap">
     
                 <div class="regular-content__background--item regular-content__background--color" style="background-color: '.$color.'"></div>
                 <div class="regular-content__background--item regular-content__background--image desktop" style="background-image: url('.$desktop_image.')"></div>
                 <div class="regular-content__background--item regular-content__background--image mobile" style="background-image: url('.$mobile_image.')"></div>
-                <div class="regular-content__background--item regular-content__background--video">
-                    ' . ( ( isset($video_url) && !empty($video_url) ) ? '<video><source src="'.$video_url.'"></video>' : '' ) . '
-                </div>
+
+    ';
+
+    // print_r($video_type);
+    // print_r($video_embed_url);
+
+    if( $video_enabled ):
+    $content .= '<div class="regular-content__background--item regular-content__background--video">';
+
+        if( ( $video_type == '1' ) && ( !empty($video_media_file) ) ):
+
+            $content .= '<video autoplay loop mute><source src="'.$video_media_file['url'].'"></video>';
+
+        elseif( ($video_type == '2' || $video_type == '3') && ( !empty($video_embed_url) ) ):
+
+            $selected_video_type = '';
+            
+            if($video_type == '2'){
+                $selected_video_type = 'youtube';
+            }elseif($video_type == '3'){
+                $selected_video_type = 'vimeo';
+            }
+
+            $content .= '<iframe src="#" data-embed-url="' . $video_embed_url . '" data-embed-source="' . $selected_video_type . '"></iframe>';
+            
+
+        endif;
+
+    $content .= '</div>';
+    endif;
+
+
+    $content .= '
                 <div class="regular-content__background--item regular-content__background--overlay" style="background: '.$overlay.'"></div>
             
             </div>
